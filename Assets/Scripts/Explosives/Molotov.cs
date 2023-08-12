b8460e7129c04010234a1e463c9d73bf69bf20d0
Parveen-Rai <praveen.rai2503@gmail.com>
2023-08-12 22:36:16 +0530

    refactor code

---
+++ Assets/Scripts/Explosives/Molotov.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace v_hero.Explosive
+{
+    public class Molotov : Explosive
+    {
+        protected override void explode()
+        {
+
+        }
+    }
+}
